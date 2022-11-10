a3d7151701c0afcdbf4c49935502030aba4d17ba
Pedro-Arantes <developer9pedro@gmail.com>
2022-11-10 02:14:15 -0300

    Version 0.2 all routes configured

--- index.js
+++ index.js
@@ -3,101 +3,203 @@ import cors from 'cors';
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import dayjs from 'dayjs'
-import  "dayjs/locale/pt-br.js ";
+
 
 //Config
 dotenv.config();
 const app = express();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db;
-const time = dayjs().locale("pt-br").format("HH:MM:SS")
+
+const time = dayjs().format("HH:mm:ss")
 app.use(cors());
 app.use(express.json());
 //Collections
-let participants;
-let messages;
-
-mongoClient.connect().then(() => {
-	db = mongoClient.db("Bate-papo_UOL_BD");
-  messages =  db.collection("messages")
-  participants = db.collection("participants")
-});
 
 
+try {
+  await mongoClient.connect();
 
+} catch (error) {
+  console.log(error)
+}
+const db = mongoClient.db("Bate-papo_UOL_BD");
+const messages = db.collection("messages")
+const participants = db.collection("participants")
 
+app.post("/participants", async (req, res) => {
 
+  const { name } = req.body
 
-app.post("/participants", (req, res) => {
-
-  const {name} = req.body
- 
-  if (!name ||  typeof name !== "string") {
+  if (!name || typeof name !== "string") {
     res.status(422).send("Todos os Campos São obrigatórios")
     return
   }
-
- 
+  const hasPart = await participants.findOne({ name })
+  if (hasPart !== null) {
+    res.sendStatus(409);
+    return
+  }
 
   const obj = {
-    name: name, 
+    name: name,
     lastStatus: Date.now()
   }
   const msg = {
-    from: name, 
-    to: 'Todos', 
-    text: 'entra na sala...', 
-    type: 'status', 
+    from: name,
+    to: 'Todos',
+    text: 'entra na sala...',
+    type: 'status',
     time: time
   }
-  messages.insertOne(msg)
-	participants.insertOne(obj).then(()=> res.sendStatus(201));
-
-});
-
-app.get("/participants", (req, res) => {
-	participants.find().toArray().then((part)=> res.send(part));
-});
-
-app.post("/messages", (req,res)=>{
-
-  const {to,text,type} = req.body;
-  const {user} = req.headers;
-  console.log(type)
-
-  const verTo = !to || typeof to !== "string" ? true:false;
-  const verText = !text || typeof text !== "string" ? true:false;
-  const verType = type !== "message " && type !== 'private_message' ? true:false;
-  
-  participants.find({name: user}).toArray().then((part)=>{
-
+  try {
+    messages.insertOne(msg)
+    await participants.insertOne(obj);
+    res.sendStatus(201)
     
-    const verFrom = part.length  <= 0 ? true:false;
-    console.log(verType )
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(error);
+  }
 
-    if (verTo|| verText ||verType||verFrom) {
+
+});
+
+app.get("/participants", async (req, res) => {
+
+  try {
+    const part = await participants.find().toArray()
+    res.send(part)
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+});
+
+app.post("/messages", async (req, res) => {
+
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+
+
+  const verTo = !to || typeof to !== "string" ? true : false;
+  const verText = !text || typeof text !== "string" ? true : false;
+  const verType = type !== "message" ? true : false;
+  //console.log(type !== 'private_message' )
+
+  try {
+
+    const part = await participants.find({ name: user }).toArray()
+    const verFrom = part.length <= 0 ? true : false;
+    if (verTo || verText || verType || verFrom) {
       res.sendStatus(422);
+      console.log(type, verTo, verText, verType, verFrom)
       return
     }
-  
+
     const msg = {
-      from: user, 
-      to: to, 
-      text: text, 
-      type: type, 
+      from: user,
+      to: to,
+      text: text,
+      type: type,
       time: time
     }
-  
-    messages.insertOne(msg).then(()=>res.sendStatus(201))
 
-  })
-  
+    await messages.insertOne(msg);
+    res.sendStatus(201);
+    return
 
-  
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(error)
+  }
+
+
 
 });
+
+app.get("/messages", async (req, res) => {
+
+  let  limit = parseInt(req.query.limit);
+  const { user } = req.headers;
+
+  if (!limit) {
+    try {
+      const msg = await messages.find({ $or: [{ from: user }, { to: user }, { to: "Todos" }] }).toArray()
+      
+      res.status(200).send(msg)
+      return
+    } catch (error) {
+      res.sendStatus(500);
+      console.log(error)
+      return
+    }
+
+  }
+  try {
+    let doc = await messages.find({ $or: [{ from: user }, { to: user }, { to: "Todos" }] }).toArray()
+    doc = doc.length;
+    const msg = await messages.find({ $or: [{ from: user }, { to: user }, { to: "Todos" }] }).skip(doc-limit).limit(limit).toArray()
+    
+    res.status(200).send(msg)
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(error)
+  }
+
+})
+
+app.post("/status", async (req, res) => {
+
+  const { user } = req.headers;
+  const lastStatus = { $set: { lastStatus: Date.now() } }
+
+  try {
+    const part = await participants.find({ name: user }).toArray()
+    participants.updateOne({ name: user }, lastStatus)
+
+    if (part.length === 0) {
+      res.sendStatus(404)
+      return
+    }
+    res.sendStatus(200)
+  } catch (error) {
+    res.sendStatus(500);
+    console.log(error)
+  }
+
+
+
+
+
+})
+
+
+setInterval( async ()=>{
+  const array = await participants.find().toArray()
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    const msg = {
+      from: element.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: time
+    }
+    console.log(Date.now() - element.lastStatus)
+    if (Date.now() - element.lastStatus  >= 10000) {
+      //console.log("oi")
+      try {
+        await participants.deleteOne(element)
+        await messages.insertOne(msg)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+}, 15000)
 
 
 app.listen(5000, () => {
-    console.log('Running on http://localhost:5000')
-  });
+  console.log('Running on http://localhost:5000')
+});
+
+
